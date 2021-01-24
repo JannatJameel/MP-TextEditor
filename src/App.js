4047c233ca85af562404386c5082c82a2bd28420
JannatJameel <j_annat@outlook.com>
2021-01-24 23:39:41 +0300

    attempt again tmr

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import {useState} from "react";
+// import styled from "styled-components";
 import "./App.css";
 
 const styles = {
-  bold: { fontWeight: "bold" },
-  italic: { fontStyle: "italic" },
-  underline: { textDecorationLine: "underline" },
+  bold: {fontWeight: "bold"},
+  italic: {fontStyle: "italic"},
+  underline: {textDecorationLine: "underline"},
 };
 
 const stylings = ["bold", "italic", "underline"];
-
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 function App() {
+  const [textColor, setTextColor] = useState("Black");
+  const [textStyle, setTextStyle] = useState("");
+  
   const stylingBoxes = stylings.map((style) => (
-    <button className="btn btn-light" style={styles[style]} key={style}>
+    <button className="btn btn-light" style={styles[style]} key={style} onClick={()=> setTextStyle(style)}>
       {style}
     </button>
   ));
@@ -21,16 +25,18 @@ function App() {
     <button
       style={{ backgroundColor: color, height: 30, width: 30 }}
       key={color}
+      onClick={()=> setTextColor(color)}
     />
   ));
 
   return (
     <div className="App">
       <div className="my-3">{stylingBoxes}</div>
-      <textarea />
+      <textarea style={{color: textColor}}/> 
       <div className="my-3">{colorBoxes}</div>
     </div>
   );
 }
 
+//styles[textStyle]
 export default App;
